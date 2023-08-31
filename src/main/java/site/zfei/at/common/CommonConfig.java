60ade158894099fb0cc12134c0041a6c657452b2
wxbty <38374721+wxbty@users.noreply.github.com>
2023-08-31 23:07:22 +0800

    增加通用分页 (#10)

--- src/main/java/site/zfei/at/common/CommonConfig.java
+++ src/main/java/site/zfei/at/common/CommonConfig.java
@@ -1,8 +1,8 @@
 package site.zfei.at.common;
 
 
+import com.github.pagehelper.PageHelper;
 import org.aspectj.lang.JoinPoint;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,18 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import site.zfei.at.common.filter.CorsFilter;
 import site.zfei.at.coxt.AtHandlerInterceptor;
 import site.zfei.at.coxt.IRequest;
+import site.zfei.at.coxt.PageAble;
 import site.zfei.at.file.AtFileConfigurationProperties;
 import site.zfei.at.trace.WebLogBean;
 
 @Configuration
 @EnableConfigurationProperties({AtFileConfigurationProperties.class})
 public class CommonConfig {
-
-    private final AtFileConfigurationProperties properties;
-
-    public CommonConfig(AtFileConfigurationProperties properties) {
-        this.properties = properties;
-    }
 
     @Bean
     public CorsFilter corsFilter() {
@@ -39,12 +34,16 @@ public class CommonConfig {
                     if (arg instanceof IRequest) {
                         ((IRequest) arg).checkParam();
                     }
+                    if (arg instanceof PageAble) {
+                        PageAble query = (PageAble) arg;
+                        PageHelper.startPage(query.getPageNo(), query.getPageSize());
+                    }
                 }
             }
 
             @Override
             public void postAfter(Object result, JoinPoint jp, WebLogBean logBean) {
-
+                PageHelper.clearPage();
             }
         };
     }
