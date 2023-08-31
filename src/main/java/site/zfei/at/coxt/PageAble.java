74994172237127d845c6f7c2a9b213c4fbf4d894
wxbty <wxb_101@163.com>
2023-08-31 23:03:46 +0800

    增加通用分页

--- src/main/java/site/zfei/at/coxt/PageAble.java
+++ src/main/java/site/zfei/at/coxt/PageAble.java
@@ -7,7 +7,7 @@ import site.zfei.at.util.Now;
 import java.io.Serializable;
 
 @Data
-public class Page implements Serializable {
+public class PageAble implements Serializable {
 
     @ApiModelProperty(value = "开始时间")
     private Long minTime = 0L;
@@ -15,18 +15,16 @@ public class Page implements Serializable {
     @ApiModelProperty(value = "结束时间")
     private Long maxTime;
 
-    @ApiModelProperty("分页条数，默认20")
-    private Integer limit = 20;
-
     private Integer orderType;
-
     private String orderCol;
 
     @ApiModelProperty("第几页")
     private Integer pageNo = 1;
 
+    @ApiModelProperty("分页条数，默认20")
+    private Integer pageSize = 20;
 
-    public Page() {
+    public PageAble() {
         this.maxTime = Now.millis();
     }
 
